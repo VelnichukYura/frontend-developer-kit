f37a6885cda5106528050b31bbe26b30db5077a1
Dmytro Baranetskyi <ckopnio@gmail.com>
2016-03-22 21:04:32 +0200

    M - gulpfile.js; M - package.json;

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ gulp.task('templates', function () {
 gulp.task('commit', function () {
   git.exec({args: 'add -A'}, function (err, stdout) {
     git.exec({args: 'diff --name-status --cached --raw'}, function (err, stdout) {
-      var message = stdout.replace(/\t/g, " - ");
+      var message = stdout.replace(/\t/g, " - ").replace(/\n/g, ";\n");
       git.exec({args: 'commit -m "' + message + '"'}, function (err, stdout) {
         console.log(err);
         console.log(stdout);
